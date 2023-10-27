dbb28e925d1fb6fac6ca9b77829ac48f9b3393f6
Дмитро Дем'яник <67798322+DemaReaktor@users.noreply.github.com>
2023-10-27 18:17:49 +0300

    create handlers

--- env/Dispatcher.py
+++ env/Dispatcher.py
@@ -1,10 +1,19 @@
 from aiogram import Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 from TelegramBot import bot
+from TTSConventer import TTSConventer
+import pathlib
 
 dispatcher = Dispatcher()
 
 
 @dispatcher.message()
 async def echo_handler(message: Message) -> None:
-    await bot.send_message(message.chat.id, "Привіт")
+    if message.content_type == ContentType.AUDIO:
+        await bot.download(message.audio, str(message.message_id) + ".mp3")
+        bot.send_message(message.chat.id, TTSConventer.convert(open(str(message.message_id) + ".mp3", 'rb')))
+        return
+    if message.content_type == ContentType.VOICE:
+        await bot.download(message.voice, str(message.message_id)+".mp3")
+        bot.send_message(message.chat.id, TTSConventer.convert(open(str(message.message_id) + ".mp3", 'rb')))
+        return
--- env/TTSConventer.py
+++ env/TTSConventer.py
@@ -1,4 +1,11 @@
+import asyncio
 import openai
 import Config
 
 openai.api_key = Config.OPENAI_API
+
+
+class TTSConventer:
+    @staticmethod
+    def convert(audio):
+        return openai.Audio.transcribe("whisper-1", audio, response_format="text")
